f54f2e2bc4a290d40878909916ecf02f58993b15
Jesus Velazquez <jesusduzz@gmail.com>
2023-01-12 17:13:36 -0600

    tal

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('hola mundo');
+
+let nombre = 'duzz'
+
+// console.log(nombre)
+
+// numero = 1
+// n2 = 3
+// suma = numero + n2
+// console.log("nombre") 
+// console.log(suma)
+
+console.log(nombre)
